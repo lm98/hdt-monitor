e4e2dd32fd7ba0a45b27db92e9429a7b01ce27e0
Leonardo Micelli <leonardomicelli@gmail.com>
2025-06-02 12:46:39 +0200

    fix: add missing attributes

--- digital-twin-wldt/app/src/main/java/org/example/model/Steps.java
+++ digital-twin-wldt/app/src/main/java/org/example/model/Steps.java
@@ -1,13 +1,23 @@
 package org.example.model;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public record Steps(
-        Integer amount,
-        Long startTime,
-        Long endTime
+        Integer count,
+        Long start_time,
+        String uid,
+        Integer start_time_zone,
+        Integer end_time_zone,
+        Long end_time,
+        Integer id
 ) {
    public static Steps defaultSteps() {
-       return new Steps(0,0L,0L);
+       return new Steps(
+               0,
+               0L,
+               "",
+               0,
+               0,
+               0L,
+               -1
+       );
    }
 }
