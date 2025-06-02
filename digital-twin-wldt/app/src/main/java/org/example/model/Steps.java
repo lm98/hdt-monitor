b6aaa6e4e83beaac71901b57da3dde3e5bb7ecea
Leonardo Micelli <leonardomicelli@gmail.com>
2025-06-02 13:59:29 +0200

    refactor(dt): remove useless fields

--- digital-twin-wldt/app/src/main/java/org/example/model/Steps.java
+++ digital-twin-wldt/app/src/main/java/org/example/model/Steps.java
@@ -2,22 +2,16 @@ package org.example.model;
 
 public record Steps(
         Integer count,
-        Long start_time,
-        String uid,
-        Integer start_time_zone,
-        Integer end_time_zone,
-        Long end_time,
-        Integer id
+        Long startTime,
+        Long endTime, Integer startTimezone,
+        Integer endTimezone
 ) {
    public static Steps defaultSteps() {
        return new Steps(
                0,
                0L,
-               "",
-               0,
-               0,
-               0L,
-               -1
+               0L, 0,
+               0
        );
    }
 }
